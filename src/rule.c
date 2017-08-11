2334db44ffcae255d96622b0857d8e52c59c8411
pachet <pachet@burninggarden.com>
2017-08-11 00:28:31 +0000

    Add basic scaffolding for HeketRule

---
+++ src/rule.c
@@ -0,0 +1,8 @@
+#include "string-array.h"
+#include "rule.h"
+
+HeketRule heket_rule_from_lines(HeketStringArray lines) {
+	HeketRule result;
+
+	return result;
+}
---
+++ src/rule.h
@@ -0,0 +1,10 @@
+#ifndef HEKET_RULE_H
+#define HEKET_RULE_H
+
+typedef struct HeketRule {
+
+} HeketRule;
+
+HeketRule heket_rule_from_lines(HeketStringArray lines);
+
+#endif
