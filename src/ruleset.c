1e81cf39d1c63f65a09c0c27fa4589056994fe06
pachet <pachet@burninggarden.com>
2017-08-11 00:29:37 +0000

    Flesh out logic for heket_ruleset_from_abnf

--- src/ruleset.c
+++ src/ruleset.c
@@ -1,9 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include "ruleset.h"
+#include "util.h"
+#include "string-array.h"
+#include "line-groups.h"
+#include "rule.h"
 
-HeketRuleset heket_ruleset_from_abnf(const char *abnf) {
+int heket_add_rule_to_ruleset(HeketRule rule, HeketRuleset ruleset)
+{
+	return 0;
+}
+
+HeketRuleset heket_ruleset_from_abnf(const char* abnf)
+{
 	HeketRuleset ruleset;
 
 	ruleset.abnf = abnf;
 
+	HeketStringArray lines = str_split(abnf, 0x0A);
+
+	if (!lines.len) {
+		return ruleset;
+	}
+
+	HeketLineGroups line_groups = heket_group_lines(lines);
+
+	int i = 0;
+
+	while (i < line_groups.len) {
+		HeketStringArray line_group = line_groups.groups[i];
+		i++;
+
+		HeketRule rule = heket_rule_from_lines(line_group);
+
+		if (!heket_add_rule_to_ruleset(rule, ruleset)) {
+			// TODO: cleanup
+		}
+	}
+
 	return ruleset;
+}
+
+
+
+
+void heket_free_ruleset(HeketRuleset ruleset) {
 }
